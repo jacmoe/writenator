bb7693656cbac3ec475a07782ca6419e5ebefb57
Jacob Moen <jacmoe.dk@gmail.com>
2018-12-02 21:24:54 +0100

    First migration

--- config/db.php
+++ config/db.php
@@ -2,13 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlite:@app/db/sqlite3.db',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
---
+++ migrations/m181202_202322_initial.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181202_202322_initial
+ */
+class m181202_202322_initial extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181202_202322_initial cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181202_202322_initial cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
