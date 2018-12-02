d1f710e06caa9bc2727d25951cef7c4b3924d42f
Jacob Moen <jacmoe.dk@gmail.com>
2018-12-02 22:02:49 +0100

    Update m181202_202322_initial.php

--- migrations/m181202_202322_initial.php
+++ migrations/m181202_202322_initial.php
@@ -7,36 +7,45 @@ use yii\db\Migration;
  */
 class m181202_202322_initial extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        echo "m181202_202322_initial cannot be reverted.\n";
-
-        return false;
-    }
-
-    /*
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
+        $this->createTable('plan', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'start' => $this->dateTime()->notNull(),
+            'end' => $this->dateTime()->notNull(),
+            'goal' => $this->integer()->notNull(),
+        ]);
 
+        $this->createTable('entry', [
+            'id' => $this->primaryKey(),
+            'plan_id' => $this->integer(),
+            'date' => $this->dateTime()->notNull(),
+            'amount' => $this->integer()->notNull(),
+        ]);
+
+        $this->createIndex(
+            'idx-entry-plan_id',
+            'entry',
+            'plan_id'
+        );
+
+        /* foreign keys not supported by Sqlite?
+        $this->addForeignKey(
+            'fk-entry-plan_id',
+            'entry',
+            'plan_id',
+            'plan',
+            'id',
+            'CASCADE'
+        );
+        */
     }
 
     public function down()
     {
         echo "m181202_202322_initial cannot be reverted.\n";
-
         return false;
     }
-    */
 }
