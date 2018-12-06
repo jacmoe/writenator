282bd6a85c842010b28095423da8b0a885198cb5
Jacob Moen <jacmoe.dk@gmail.com>
2018-12-06 03:29:29 +0100

    Major asset clean up

--- views/layouts/partials/_header.php
+++ views/layouts/partials/_header.php
@@ -3,11 +3,11 @@
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-        'class' => 'navbar navbar-dark bg-dark navbar-static-top navbar-frontend',
+        'class' => 'navbar navbar-dark bg-dark navbar-static-top navbar-expand-lg navbar-frontend',
         ],
     ]);
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav mr-auto'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
         ],
--- views/layouts/partials/_top.php
+++ views/layouts/partials/_top.php
@@ -7,7 +7,6 @@ use app\widgets\Alert;
 use yii\helpers\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($view);
